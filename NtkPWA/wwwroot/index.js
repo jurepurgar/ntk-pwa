cb053bc8c86d3f5f0d14c0d78865cba87a94f58b
Jure Purgar <jure@purgar.net>
2019-05-15 19:54:18 +0200

    commit

---
+++ NtkPWA/wwwroot/index.js
@@ -0,0 +1,18 @@
+﻿if ('serviceWorker' in navigator) {
+    console.log("registering Service Worker");
+    navigator.serviceWorker.register('sw.js');
+}
+
+//sw push: https://medium.com/izettle-engineering/beginners-guide-to-web-push-notifications-using-service-workers-cb3474a17679
+
+
+
+/*
+document.querySelector('#show').addEventListener('click', () => {
+    const iconUrl = document.querySelector('select').selectedOptions[0].value;
+    let imgElement = document.createElement('img');
+    imgElement.src = iconUrl;
+    document.querySelector('#container').appendChild(imgElement);
+});
+
+*/
