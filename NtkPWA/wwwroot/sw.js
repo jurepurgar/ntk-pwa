812e4f3604e7cf00b2ebc43cecc246b0924e6833
Jure Purgar <jure@purgar.net>
2019-05-19 16:23:27 +0200

    add offline features

--- NtkPWA/wwwroot/sw.js
+++ NtkPWA/wwwroot/sw.js
@@ -14,8 +14,47 @@ self.addEventListener('push', async function (event) {
 });
 
 
+
+
 function showLocalNotification(message) {
     this.registration.showNotification(message.Sender, {
         body: message.Text
+    });
+}
+
+
+//offline
+const CACHE = "offline-cache";
+
+// If any fetch fails, it will look for the request in the cache and serve it from there first
+self.addEventListener("fetch", function (event) {
+    if (event.request.method !== "GET") return;
+    event.respondWith(
+        fetch(event.request)
+            .then(function (response) {
+                // If request was success, add or update it in the cache
+                event.waitUntil(updateCache(event.request, response.clone()));
+                return response;
+            })
+            .catch(function (error) {
+                return fromCache(event.request);
+            })
+    );
+});
+
+function fromCache(request) {
+    return caches.open(CACHE).then(cache => {
+        return cache.match(request).then(function (matching) {
+            if (!matching || matching.status === 404) {
+                return Promise.reject("no-match");
+            }
+            return matching;
+        });
+    });
+}
+
+function updateCache(request, response) {
+    return caches.open(CACHE).then(function (cache) {
+        return cache.put(request, response);
     });
 }
